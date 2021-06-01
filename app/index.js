1b5c13290556dd1a5f2eece47172b53b8df098c9
Lokesh <53149949+Lokeshdroid@users.noreply.github.com>
2021-06-01 22:37:07 +0530

    New Update

--- app/index.js
+++ app/index.js
@@ -99,7 +99,7 @@ Typer.speed = 3;
 Typer.file = "app/data/lokesh.txt";
 Typer.init();
 
-var timer = setInterval("t();", 20);
+var timer = setInterval("t();",20);
 function t() {
   Typer.addText({ keyCode: 123748 });
 
